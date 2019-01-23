e15541fe4c12a65247f690169389896f32e404fd
Unknown <j.ahameafule@gmx.ch>
2019-01-23 12:14:19 +0100

    init

---
+++ src/main/java/ch/noseryoung/demoplus/DemoplusApplication.java
@@ -0,0 +1,14 @@
+package ch.noseryoung.demoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoplusApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/ch/noseryoung/demoplus/DemoplusApplicationTests.java
@@ -0,0 +1,17 @@
+package ch.noseryoung.demoplus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoplusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
