b64cd258da49fae418c5e0da46e5c4137335c1a2
Unknown <j.ahameafule@gmx.ch>
2019-01-23 12:15:53 +0100

    update

--- src/main/java/ch/noseryoung/demoplus/DemoplusApplication.java
+++ src/main/java/ch/noseryoung/demoplus/DemoplusApplication.java
@@ -2,8 +2,10 @@ package ch.noseryoung.demoplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class DemoplusApplication {
 
 	public static void main(String[] args) {
